31a73dabaa0d670ddbb55c65bb5618fe9b9fd9de
Ke Dat <datmartin10@gmail.com>
2025-07-27 16:13:32 +0700

    Refactor folder

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import {
   getFirestore,
   collection,
@@ -15,27 +15,27 @@ import {
   getDoc,
 } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyCmqL5PC6vil-1pU2xWkbMuS3w4QVFQ9kA",
-   authDomain: "jsi05-35a7b.firebaseapp.com",
-   databaseURL: "https://jsi05-35a7b-default-rtdb.firebaseio.com",
-   projectId: "jsi05-35a7b",
-   storageBucket: "jsi05-35a7b.firebasestorage.app",
-   messagingSenderId: "881389229214",
-   appId: "1:881389229214:web:a4eec1039aaf45c633578f",
-   measurementId: "G-9QD9TD3KG0"
- };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmqL5PC6vil-1pU2xWkbMuS3w4QVFQ9kA",
+  authDomain: "jsi05-35a7b.firebaseapp.com",
+  databaseURL: "https://jsi05-35a7b-default-rtdb.firebaseio.com",
+  projectId: "jsi05-35a7b",
+  storageBucket: "jsi05-35a7b.firebasestorage.app",
+  messagingSenderId: "881389229214",
+  appId: "1:881389229214:web:a4eec1039aaf45c633578f",
+  measurementId: "G-9QD9TD3KG0"
+};
 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
- console.log(app)
- // Firebase SDK
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(app)
+// Firebase SDK
 
 
 async function displayProducts() {
@@ -47,7 +47,6 @@ async function displayProducts() {
   querySnapshot.forEach((doc) => {
     const product = doc.data();
     console.log("🚀 ~ querySnapshot.forEach ~ product:", product)
-    
 
     // Create a swiper-slide element
     const slide = document.createElement("div");
