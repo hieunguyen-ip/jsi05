fe14777253e2fe9a2a7598af74b653f7ab3c00db
Hieu <soimedusak9@gmail.com>
2025-07-27 15:25:20 +0700

    update all exam project

--- Hieu/firebase-config.js
+++ Hieu/firebase-config.js
@@ -2,6 +2,19 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +35,40 @@
  const app = initializeApp(firebaseConfig);
  const analytics = getAnalytics(app);
  console.log(app)
+ // Firebase SDK
+
+
+async function displayProducts() {
+  const productCollection = collection(db, "xoai");
+  const querySnapshot = await getDocs(productCollection);
+
+  const wrapper = document.querySelector(".game-list-1");
+
+  querySnapshot.forEach((doc) => {
+    const product = doc.data();
+    console.log("🚀 ~ querySnapshot.forEach ~ product:", product)
+    
+
+    // Create a swiper-slide element
+    const slide = document.createElement("div");
+    slide.classList.add("game");
+
+    // Add product content to the slide
+    slide.innerHTML = `
+                ￼
+                <h4>${product.Name}</h4>
+                <p>${product.Information}</p>
+                <p class="price">$${product.Price} USD</p>
+                <a href="product-detail.html?game=${product.url}" class="buy-button">Làm bài ngay</a>
+  `;
+
+    // Append the slide to the swiper-wrapper
+    wrapper.appendChild(slide);
+  });
+
+  // Reinitialize Swiper after adding slides
+
+}
+
+// Call the function to display products
+displayProducts();
